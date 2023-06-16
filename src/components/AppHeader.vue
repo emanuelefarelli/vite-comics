a1b84d0f1c99f07465541e307c00a511f00e8a51
emanuelefarelli <emaaliens888@gmail.com>
2023-06-16 09:55:06 +0200

    Added basic structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+    export default{
+        name: 'AppNav',
+        data(){
+
+        }
+    }
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+
+</style>
